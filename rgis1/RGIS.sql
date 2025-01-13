3dfa2f3aa9c1adcc513e86140fb54ee8cf48cdf3
D00men <domen.fafulic@student.um.si>
2025-01-13 23:34:58 +0100

    ajde15

---
+++ rgis1/RGIS.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE RGIS;
+DROP TABLE IF EXISTS __EFMigrationsHistory;
+DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS users;
+DESCRIBE events;
+
+select * from users;
+select * from events;
